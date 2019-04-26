77cd458a654d1d766a061a403cee5ae66baed7df
Simon Bond <sjb277@medschl.cam.ac.uk>
2019-04-26 14:41:11 +0100

    overall push to create r library

--- R/R example/README.txt
+++
@@ -1,9 +0,0 @@
-Open talk_example.R and edit the first line to point to the containing directory. 
-
-The eudract_tools.R file defines several useful functions. You made need to install some external packageds (tidyr, dplyr, xslt, and their dependencies)
-
-The file safety.rda contains an example of raw data, to be replaced with your own data.
-
-If run correctly then it will produce 2 new files within the same directory:  simple.xml table_eudract.xml. The final one being able to be uploaded into eudraCT. Compare to the files in the "reference output" directory, which is what your output should be. 
-
-You will also have within your R environment a data frame soc_code with three columns giving a complete list of: SOC terms in English, the EudraCT coding values, the MedDRA coding values. This is to be used for linking as needed.
---
+++ R/eudract.Rcheck/00_pkg_src/eudract/R/eudract_convert.R
@@ -0,0 +1,29 @@
+#' applies a conversion using xslt from a simple xml file to a eudract compatible file, and checks against the schema
+#'
+#' @param input a character string giving the file path to the simple xml file
+#' @param ouput a character string naming the output file
+#' @param xslt a character string giving the file path to the xslt script. Defaults to the script provided in this package
+#' @param schema_output a character string giving the file path to the schema. A copy was downloaded and is provided in this package as the default.
+#'
+#' @return the output from the validation against the schema. A new file is created as a side-effect, which is suitable to upload into eudraCT.
+#'
+#' @export
+#' @examples
+#'safety_statistics <- safety_summary(safety, exposed=c("Experimental"=60,"Control"=67))
+#'simple_safety_xml(safety_statistics, "simple.xml")
+#'eudract_convert(input="simple.xml", output="table_eudract.xml")
+
+
+
+eudract_convert <- function(input, output="table_eudract.xml",
+                            xslt=system.file("extdata","simpleToEudraCT.xslt", package="eudract"),
+                            schema_output=system.file("extdata","adverseEvents.xsd", package="eudract")
+){
+  doc <- xml2::read_xml(input)
+  style <- xml2::read_xml(xslt)
+  schema <- xml2::read_xml(schema_output)
+  output_xml <- xslt::xml_xslt(doc, style)
+  xml2::write_xml(output_xml, output)
+  cat(paste(output, "is created or modified\nValidation Status:\n"))
+  xml2::xml_validate(output_xml, schema)
+}
