8b711a0603285cdd61034c6f8dc1b2d717d8b816
BPantaleo <52530179+BPantaleo@users.noreply.github.com>
2019-07-31 14:04:26 +0100

    updated

--- testing/InputData_TestScript_R.r
+++ testing/InputData_TestScript_R.r
@@ -10,8 +10,20 @@ any(colnames(test_groupdata) == "subjectsAffectedByNonSeriousAdverseEvents")
 any(colnames(test_groupdata) == "subjectsExposed")
 any(colnames(test_groupdata) == "deathsAllCauses")
 
-#Check the NONSERIOUS file contains exactely the following variables: groupTitle, subjectsAffected, occurrences, term, eutctId
+#Check the variable title in the GROUP file is a string >=4 digits long
+test_groupdata <- read.csv(file="GROUP.csv")
+group_title<-test_groupdata$title
+library(stringi)
 
+for (value in group_title){
+	if(stri_length(value)>3) {
+		print("Value is longer than 3 digits")
+	} else {
+		print(value)
+	}
+}
+
+#Check the NONSERIOUS file contains exactely the following variables: groupTitle, subjectsAffected, occurrences, term, eutctId
 test_nonseriousdata <- read.csv(file="NONSERIOUS.csv")
 any(colnames(test_nonseriousdata) == "groupTitle")
 any(colnames(test_nonseriousdata) == "subjectsAffected")
@@ -65,4 +77,6 @@ any(colnames(test_eventsdata) == "serious")
 any(colnames(test_eventsdata) == "related")
 any(colnames(test_eventsdata) == "fatal")
 any(colnames(test_eventsdata) == "group")
+
+
 
